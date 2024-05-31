e93cd732021a87aa62d0f6071d832dce2d734b34
Mattias Lightstone <87032785+mattiaslightstone@users.noreply.github.com>
2024-05-31 09:52:18 -0400

    Add title to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 "use client";
 import { NavLinkButton } from "@/components/Buttons/NavLinkButton";
-import { Flex } from "@mantine/core";
+import { Flex, Title } from "@mantine/core";
 
 export default function Home() {
   return (
     <Flex style={{ height: "100vh" }} align="center" justify="center">
-      <div>
-        <NavLinkButton label="Create" path="/create" />
-        <NavLinkButton label="Manage" path="/manage" />
-      </div>
+      <Flex direction="column" align="center" gap={24}>
+        <Title order={3}>Start Streaming Now!</Title>
+        <div>
+          <NavLinkButton label="Create" path="/create" />
+          <NavLinkButton label="Manage" path="/manage" />
+        </div>
+      </Flex>
     </Flex>
   );
 }
