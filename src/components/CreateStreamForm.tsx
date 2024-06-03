ad108b13e5ff0751292c1e21912f50c6729d2f62
Mattias Lightstone <87032785+mattiaslightstone@users.noreply.github.com>
2024-06-03 10:29:09 -0400

    Update create stream form labels

--- src/components/CreateStreamForm.tsx
+++ src/components/CreateStreamForm.tsx
@@ -2,12 +2,16 @@ import {
   ActionIcon,
   Button,
   Card,
+  CardSection,
+  CardSectionProps,
+  Divider,
   Flex,
   Loader,
   NumberInput,
   Select,
   Text,
   TextInput,
+  TextProps,
   Title,
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
@@ -112,78 +116,119 @@ export const CreateStreamForm = () => {
   };
 
   return (
-    <Card bg={"cardBackground"}>
-      <form onSubmit={form.onSubmit(handleSubmit)}>
-        <Flex
-          direction={"column"}
-          align={"right"}
-          justify={"center"}
-          gap={20}
-          style={{ color: "white" }}
-        >
-          {coins && (
-            <Select
-              label={"What token do you want to use?"}
-              placeholder="Pick Token"
-              data={coins.map((coin) => ({
-                label: coin.assetId || "Unknown",
-                // label: coin.symbol || coin.address || 'Unknown', // Fallback to 'Unknown' if symbol is undefined
-                value: coin.assetId.toString(), // Assuming address is the desired value
-              }))}
-              {...form.getInputProps("token")}
+    <Card bg={"cardBackground"} p="lg">
+      <CustomCardSection>
+        <Title order={4}>Create Stream</Title>
+      </CustomCardSection>
+
+      <CustomCardSection>
+        <form onSubmit={form.onSubmit(handleSubmit)}>
+          <Flex
+            direction={"column"}
+            align={"right"}
+            justify={"center"}
+            gap={20}
+            style={{ color: "white" }}
+          >
+            {coins && (
+              <Select
+                label={
+                  <CustomLabelComponent>
+                    What token do you want to use?
+                  </CustomLabelComponent>
+                }
+                placeholder="Pick Token"
+                data={coins.map((coin) => ({
+                  label: coin.assetId || "Unknown",
+                  // label: coin.symbol || coin.address || 'Unknown', // Fallback to 'Unknown' if symbol is undefined
+                  value: coin.assetId.toString(), // Assuming address is the desired value
+                }))}
+                {...form.getInputProps("token")}
+              />
+            )}
+            <NumberInput
+              label={
+                <CustomLabelComponent>
+                  How much do you want to stream in total?
+                </CustomLabelComponent>
+              }
+              placeholder={"100"}
+              {...form.getInputProps("amount")}
             />
-          )}
-          <NumberInput
-            label={"How much do you want to stream in total?"}
-            placeholder={"100"}
-            {...form.getInputProps("amount")}
-          />
-          <TextInput
-            label={"Who is the recipient?"}
-            placeholder={"0x12345.."}
-            {...form.getInputProps("recipient")}
-          />
-          {/* TODO: add maximum and minimum dates */}
-          <DatesProvider settings={{ timezone: timezone }}>
-            <Flex direction="column">
-              <Flex direction="column" gap={3}>
-                <DatePickerInput
-                  type="range"
-                  clearable
-                  label={
-                    <Flex align="center">
-                      Start and End Dates
-                      <ActionIcon
-                        onClick={openTzModal}
-                        variant="subtle"
-                        size="sm"
-                      >
-                        <IconSettings size={14} />
-                      </ActionIcon>
-                    </Flex>
-                  }
-                  valueFormat="MMMM DD, YYYY HH:mm"
-                  {...form.getInputProps("dates")}
-                />
+            <TextInput
+              label={
+                <CustomLabelComponent>
+                  Who is the recipient?
+                </CustomLabelComponent>
+              }
+              placeholder={"0x12345.."}
+              {...form.getInputProps("recipient")}
+            />
+            {/* TODO: add maximum and minimum dates */}
+            <DatesProvider settings={{ timezone: timezone }}>
+              <Flex direction="column">
+                <Flex direction="column" gap={3}>
+                  <DatePickerInput
+                    type="range"
+                    clearable
+                    label={
+                      <Flex align="center">
+                        <CustomLabelComponent
+                          icon={
+                            <ActionIcon
+                              onClick={openTzModal}
+                              variant="subtle"
+                              size="sm"
+                            >
+                              <IconSettings size={14} />
+                            </ActionIcon>
+                          }
+                        >
+                          Start and End Dates
+                        </CustomLabelComponent>
+                      </Flex>
+                    }
+                    valueFormat="MMMM DD, YYYY"
+                    {...form.getInputProps("dates")}
+                  />
+                </Flex>
               </Flex>
-            </Flex>
-          </DatesProvider>
-          {wallet.wallet ? (
-            <Button type="submit" fz={"lg"}>
-              Create stream
-            </Button>
-          ) : isConnecting ? (
-            <Button>
-              <Loader color={"white"} size={"sm"} />
-            </Button>
-          ) : (
-            <Button onClick={connect} loading={isConnecting} fz={"lg"}>
-              Connect Wallet To Create
-            </Button>
-          )}
-        </Flex>
-      </form>
+            </DatesProvider>
+            {wallet.wallet ? (
+              <Button type="submit" fz={"lg"}>
+                Create stream
+              </Button>
+            ) : isConnecting ? (
+              <Button>
+                <Loader color={"white"} size={"sm"} />
+              </Button>
+            ) : (
+              <Button onClick={connect} loading={isConnecting} fz={"lg"}>
+                Connect Wallet To Create
+              </Button>
+            )}
+          </Flex>
+        </form>
+      </CustomCardSection>
       <TimezoneModal opened={tzModalOpened} onClose={closeTzModal} />
     </Card>
   );
 };
+
+const CustomCardSection = (
+  props: CardSectionProps & React.HTMLAttributes<HTMLDivElement>,
+) => {
+  return <CardSection inheritPadding withBorder py="md" {...props} />;
+};
+
+const CustomLabelComponent = ({
+  icon,
+  children,
+  ...props
+}: TextProps &
+  React.HTMLAttributes<HTMLDivElement> & { icon?: React.ReactNode }) => (
+  <Flex align="center" mb="sm">
+    <Text {...props}>{children}</Text>
+    {icon}
+  </Flex>
+);
