b46e4d34f5c5eb3b55773b576b55cd742962400b
EndlessLucky <105.olivesoft@gmail.com>
2024-09-10 00:19:00 -0400

    feat - remove the indexer from the next build

--- src/hooks/useStreamProvider.ts
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import { StreamContext } from "./StreamProvider";
-
-const useStreamProvider = () => useContext(StreamContext);
-
-export default useStreamProvider;
