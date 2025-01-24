328426417a05ebd2c573ff62ded7e5de2ff319e3
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2025-01-24 18:22:43 +0100

    feat: wallet dropdown displays account balance

--- src/components/Wallet/WalletView.tsx
+++ src/components/Wallet/WalletView.tsx
@@ -1,9 +1,23 @@
 import React from "react";
-import { useDisconnect } from "@fuels/react";
-import { Avatar, Button, MantineColor } from "@mantine/core";
+import { useBalance } from "@fuels/react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Card,
+  Flex,
+  MantineColor,
+  Popover,
+  Text,
+  Image,
+} from "@mantine/core";
 import { IconWallet } from "@tabler/icons-react";
 import { formatAddress } from "utils/formatUtils";
 import { TextLg } from "components/TextVariants";
+import { BN } from "fuels";
+import ETHLogo from "@/assets/ETH.png";
+import NextImage from "next/image";
+import useEthConverter from "@/hooks/useETHConverter";
 
 type WalletProps = {
   isConnected: boolean;
@@ -15,6 +29,14 @@ type WalletProps = {
   color?: MantineColor;
 };
 
+const formatMoney = (amount: number) => {
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+  return formatter.format(amount);
+};
+
 export const WalletView = ({
   isConnected,
   connectAsync,
@@ -22,22 +44,69 @@ export const WalletView = ({
   account = "",
   color = "white",
 }: WalletProps) => {
-  const { disconnect } = useDisconnect();
+  const { balance } = useBalance({ address: account });
+  const accountBalance = new BN(balance);
+  const { usdAmount } = useEthConverter({
+    initialEthAmount: accountBalance.toString(),
+  });
 
   return (
     <div>
       {isConnected ? (
-        <Button
-          radius={"999px"}
-          h={"32px"}
-          color="cardBackground"
-          onClick={() => disconnect()}
-          p={"xs"}
-          pr={"md"}
-        >
-          <Avatar color={color} size={24}></Avatar>
-          <TextLg c={color}>{formatAddress(account ?? "")}</TextLg>
-        </Button>
+        <Popover width={200} position="bottom" withArrow shadow="md">
+          <Popover.Target>
+            <Button
+              radius={"999px"}
+              h={"32px"}
+              color="cardBackground"
+              p={"xs"}
+              pr={"md"}
+            >
+              <Avatar color={color} size={24}></Avatar>
+              <TextLg c={color}>{formatAddress(account ?? "")}</TextLg>
+            </Button>
+          </Popover.Target>
+
+          <Popover.Dropdown>
+            <Card p="xs">
+              <Text>Total balance</Text>
+
+              <Box
+                my="lg"
+                py="md"
+                component="div"
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  borderRadius: "12px",
+                  background:
+                    "radial-gradient(100% 225% at 0 0, #1a1a1a 0%, #29483a 100%)",
+                }}
+              >
+                <Box mb="sm">
+                  <Image component={NextImage} src={ETHLogo} alt="" />
+                </Box>
+                <Flex>
+                  <Text
+                    size="xl"
+                    mr="sm"
+                    style={{
+                      color: "green",
+                    }}
+                  >
+                    {accountBalance.format()}
+                  </Text>
+                  <Text size="xl">ETH</Text>
+                </Flex>
+                <Flex>
+                  <Text>{formatMoney(parseInt(usdAmount))}</Text>
+                </Flex>
+              </Box>
+            </Card>
+          </Popover.Dropdown>
+        </Popover>
       ) : (
         <Button
           radius={"999px"}
